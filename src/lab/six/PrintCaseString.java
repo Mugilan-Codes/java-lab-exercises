0f82a26d5e56ad273308564a8171022ac060bc1a
Mugilan-Codes <mugilancodes@gmail.com>
2020-09-29 21:40:14 +0530

    Print Changed Cases

--- src/lab/six/PrintCaseString.java
+++ src/lab/six/PrintCaseString.java
@@ -1,12 +1,23 @@
 package lab.six;
 
+import java.util.Scanner;
+
 public class PrintCaseString {
 
-    public static void main(String[] args) {
-        String str = "Hi this AA tEst";
+    private static void printChangedCase(String str) {
+        System.out.println("\nGiven String: " + str);
+        System.out.println("Uppercase String: " + str.toUpperCase());
+        System.out.println("Lowercase String: " + str.toLowerCase());
+    }
 
-        System.out.println(str.toUpperCase());
-        System.out.println(str.toLowerCase());
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Print Given string in Upper and Lower Cases");
+        System.out.print("Enter a string: ");
+        String str = in.nextLine();
+
+        printChangedCase(str);
     }
 
 }
